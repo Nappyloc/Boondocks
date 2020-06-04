54a1b5a61d5d8e794b83ba8f65dfaa24469496d3
Dania King <dania@fortunebuilders.com>
2020-06-04 16:40:06 -0700

    Updates

---
+++ precache-manifest.439c0eceef13345c37ca2d8373c535f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d782d56e1076c816828d851e48f008",
+    "url": "/Boondocks/index.html"
+  },
+  {
+    "revision": "c468af93a915a591c867",
+    "url": "/Boondocks/static/css/main.7fe28f50.chunk.css"
+  },
+  {
+    "revision": "48fd4f9fa6425f06aebb",
+    "url": "/Boondocks/static/js/2.123fed46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Boondocks/static/js/2.123fed46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c468af93a915a591c867",
+    "url": "/Boondocks/static/js/main.2133daf2.chunk.js"
+  },
+  {
+    "revision": "2bb59b695e7af307ad72",
+    "url": "/Boondocks/static/js/runtime-main.dc4a7277.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44cb8660518500510c2df6a290eed4d7.js"
+  "/Boondocks/precache-manifest.439c0eceef13345c37ca2d8373c535f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Boondocks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

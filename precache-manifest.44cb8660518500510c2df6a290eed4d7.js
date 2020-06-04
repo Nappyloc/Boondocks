2d915a9224cda7a37480dd48dd42d098e10c9f50
Dania King <dania@fortunebuilders.com>
2020-06-04 16:37:36 -0700

    Updates

---
+++ precache-manifest.44cb8660518500510c2df6a290eed4d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5f8387f8dc2a408f0f061ed89c87be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "028cfc21834c302f25c3",
+    "url": "/static/css/main.7fe28f50.chunk.css"
+  },
+  {
+    "revision": "48fd4f9fa6425f06aebb",
+    "url": "/static/js/2.123fed46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.123fed46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028cfc21834c302f25c3",
+    "url": "/static/js/main.4535d009.chunk.js"
+  },
+  {
+    "revision": "327ee23e941d7c091ab0",
+    "url": "/static/js/runtime-main.8e209444.js"
+  }
+]);
